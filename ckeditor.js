f1a11eee5e9c74e3fec9576dea41f49b61b0fb51
Youssef Attia <yattia@google.com>
2023-07-26 20:57:20 +0000

    Edited to use htmlSafeByReview

--- ckeditor.js
+++ ckeditor.js
@@ -25,20 +25,7 @@ else {
 		script.src = CKEDITOR.getUrl( 'core/loader.js' );
 		document.body.appendChild( script );
 	} else {
-		if (self.trustedTypes && self.trustedTypes.createPolicy) {
-			// The CKEDITOR.getUrl function should be safe since the basePath calculated in ckeditor_base.js is pulled from the script elements on the page or is developer controlled by a window flag.
-			const policy = self.trustedTypes.createPolicy(
-				'ckeditor#constructor',
-				{
-					createHTML: function (html) {
-						return html;
-					},
-				}
-			);
-			document.write(policy.createHTML('<script type="text/javascript" src="' + CKEDITOR.getUrl('core/loader.js') + '"></script>'));
-		} else {
-			document.write('<script type="text/javascript" src="' + CKEDITOR.getUrl('core/loader.js') + '"></script>');
-		}
+		document.write(CKEDITOR.tools.htmlSafeByReview('<script type="text/javascript" src="' + CKEDITOR.getUrl('core/loader.js') + '"></script>', 'The CKEDITOR.getUrl function should be safe since the basePath calculated in ckeditor_base.js is pulled from the script elements on the page or is developer controlled by a window flag.'));
 	}
 
 }
